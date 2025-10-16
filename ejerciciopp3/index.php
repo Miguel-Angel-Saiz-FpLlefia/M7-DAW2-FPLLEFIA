f6f3b8da3fd2e4e86b316393afee7ede51348bfd
Miguel-Angel-Saiz-FpLlefia <saizangullomiguelangel@fpllefia.com>
2025-10-16 16:56:29 +0000

    change the arrya file

---
+++ ejerciciopp3/index.php
@@ -0,0 +1,148 @@
+<?php
+    $peliculas = [
+        [
+            "nom" => "Inception",
+            "imatge" => "https://m.media-amazon.com/images/I/912AErFSBHL._UF1000,1000_QL80_.jpg",
+            "horaris" => ["16:00", "19:30", "22:00"],
+            "sinopsi" => "Un lladre que roba secrets a través de la tecnologia d'infiltració dels somnis rep l'encàrrec invers: implantar una idea en la ment d'un altre.",
+            "durada" => "148 min",
+            "director" => "Christopher Nolan",
+            "repartiment" => ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
+            "qualificacio" => "+16",
+            "genere" => "Ciència ficció, Thriller",
+            "url_trailer" => "https://www.youtube.com/watch?v=YoHD9XEInc0"
+        ],
+        [
+            "nom" => "The Shawshank Redemption",
+            "imatge" => "https://m.media-amazon.com/images/I/51NiGlapXlL._AC_.jpg",
+            "horaris" => ["15:00", "18:00", "21:00"],
+            "sinopsi" => "Dos homes empresonats formen una profunda amistat mentre intenten mantenir l’esperança i la llibertat en una presó de màxima seguretat.",
+            "durada" => "142 min",
+            "director" => "Frank Darabont",
+            "repartiment" => ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
+            "qualificacio" => "+16",
+            "genere" => "Drama",
+            "url_trailer" => "https://www.youtube.com/watch?v=6hB3S9bIaco"
+        ],
+        [
+            "nom" => "Parasite",
+            "imatge" => "https://m.media-amazon.com/images/I/71c05lTE03L._AC_SY679_.jpg",
+            "horaris" => ["17:00", "20:00", "22:30"],
+            "sinopsi" => "La família Kim s'infiltra a la vida d'una família rica, però les coses es compliquen en un joc de poder i supervivència.",
+            "durada" => "132 min",
+            "director" => "Bong Joon-ho",
+            "repartiment" => ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
+            "qualificacio" => "+16",
+            "genere" => "Thriller, Drama",
+            "url_trailer" => "https://www.youtube.com/watch?v=5xH0HfJHsaY"
+        ],
+        [
+            "nom" => "The Dark Knight",
+            "imatge" => "https://m.media-amazon.com/images/S/pv-target-images/e9a43e647b2ca70e75a3c0af046c4dfdcd712380889779cbdc2c57d94ab63902.jpg",
+            "horaris" => ["16:30", "19:45", "22:15"],
+            "sinopsi" => "Batman s'enfronta a l'aparició del Joker, un criminal anarquista que amenaça la ciutat de Gotham.",
+            "durada" => "152 min",
+            "director" => "Christopher Nolan",
+            "repartiment" => ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Thriller",
+            "url_trailer" => "https://www.youtube.com/watch?v=EXeTwQWrcwY"
+        ],
+        [
+            "nom" => "Interstellar",
+            "imatge" => "https://m.media-amazon.com/images/I/91EtQnm1J-L._UF1000,1000_QL80_.jpg",
+            "horaris" => ["15:30", "18:30", "21:30"],
+            "sinopsi" => "Un grup d'exploradors utilitza un nou túnel espacial per superar les limitacions humanes i salvar la humanitat.",
+            "durada" => "169 min",
+            "director" => "Christopher Nolan",
+            "repartiment" => ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
+            "qualificacio" => "+16",
+            "genere" => "Ciència ficció, Drama",
+            "url_trailer" => "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+        ],
+        [
+            "nom" => "Joker",
+            "imatge" => "https://aficine.com/wp-content/uploads/2024/07/ES_SPAIN_JOKER2_VERT_MAIN_2764x4096_INTL-691x1024.jpg",
+            "horaris" => ["17:15", "20:15", "22:45"],
+            "sinopsi" => "Un home fracassat i marginat es converteix en el famós i terrorífic Joker.",
+            "durada" => "122 min",
+            "director" => "Todd Phillips",
+            "repartiment" => ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
+            "qualificacio" => "+16",
+            "genere" => "Drama, Thriller",
+            "url_trailer" => "https://www.youtube.com/watch?v=zAGVQLHvwOY"
+        ],
+        [
+            "nom" => "Avengers: Endgame",
+            "imatge" => "https://m.media-amazon.com/images/I/81ExhpBEbHL._AC_SY679_.jpg",
+            "horaris" => ["14:30", "18:00", "21:00"],
+            "sinopsi" => "Els venjadors restants intenten revertir les conseqüències del Snap de Thanos.",
+            "durada" => "181 min",
+            "director" => "Anthony Russo, Joe Russo",
+            "repartiment" => ["Robert Downey Jr.", "Chris Evans", "Scarlett Johansson"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Ciència ficció",
+            "url_trailer" => "https://www.youtube.com/watch?v=TcMBFSGVi1c"
+        ],
+        [
+            "nom" => "La La Land",
+            "imatge" => "https://play-lh.googleusercontent.com/9WLX4kpGokn73nXMUAElCj0byelBhVPSM5vbskzxZuMH8_guUJIbzSknr4wJ_8jRoZhVS9YRIWdunr9HMLLb=w240-h480-rw",
+            "horaris" => ["16:00", "19:00", "21:30"],
+            "sinopsi" => "Un músic i una actriu aspirant esdevenen amants a Los Angeles mentre persegueixen els seus somnis.",
+            "durada" => "128 min",
+            "director" => "Damien Chazelle",
+            "repartiment" => ["Ryan Gosling", "Emma Stone", "John Legend"],
+            "qualificacio" => "+16",
+            "genere" => "Musical, Romance",
+            "url_trailer" => "https://www.youtube.com/watch?v=0pdqf4P9MB8"
+        ],
+        [
+            "nom" => "The Godfather",
+            "imatge" => "https://m.media-amazon.com/images/I/41+eK8zBwQL._AC_.jpg",
+            "horaris" => ["15:00", "18:30", "21:00"],
+            "sinopsi" => "La història de la família Corleone, una de les més poderoses famílies mafioses d'Amèrica.",
+            "durada" => "175 min",
+            "director" => "Francis Ford Coppola",
+            "repartiment" => ["Marlon Brando", "Al Pacino", "James Caan"],
+            "qualificacio" => "+16",
+            "genere" => "Crim, Drama",
+            "url_trailer" => "https://www.youtube.com/watch?v=sY1S34973zA"
+        ],
+        [
+            "nom" => "Fight Club",
+            "imatge" => "https://m.media-amazon.com/images/I/51v5ZpFyaFL._AC_.jpg",
+            "horaris" => ["17:00", "20:00", "22:30"],
+            "sinopsi" => "Un home descontent amb la seva vida crea un club de lluita clandestí que canviarà la seva percepció del món.",
+            "durada" => "139 min",
+            "director" => "David Fincher",
+            "repartiment" => ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
+            "qualificacio" => "+16",
+            "genere" => "Drama, Thriller",
+            "url_trailer" => "https://www.youtube.com/watch?v=SUXWAEX2jlg"
+        ],
+        [
+            "nom" => "Gladiator",
+            "imatge" => "https://es.web.img3.acsta.net/medias/nmedia/18/70/92/02/20149073.jpg",
+            "horaris" => ["15:30", "18:30", "21:00"],
+            "sinopsi" => "Un general romà traït es converteix en gladiador per buscar venjança contra l'emperador corrupte.",
+            "durada" => "155 min",
+            "director" => "Ridley Scott",
+            "repartiment" => ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen"],
+            "qualificacio" => "+16",
+            "genere" => "Acció, Drama, Aventura",
+            "url_trailer" => "https://www.youtube.com/watch?v=owK1qxDselE"
+        ],
+        [
+            "nom" => "The Matrix",
+            "imatge" => "https://m.media-amazon.com/images/I/51vpnbwFHrL._AC_.jpg",
+            "horaris" => ["16:00", "19:00", "22:00"],
+            "sinopsi" => "Un hacker descobreix la veritat sobre la realitat i es converteix en el líder de la rebel·lió contra les màquines.",
+            "durada" => "136 min",
+            "director" => "Lana Wachowski, Lilly Wachowski",
+            "repartiment" => ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+            "qualificacio" => "+16",
+            "genere" => "Ciència ficció, Acció",
+            "url_trailer" => "https://www.youtube.com/watch?v=vKQi3bBA1y8"
+        ]
+    ];
+?>
