55d3d847e9ec6b2f8d4a21e358cba14109843e0d
Miguel-Angel-Saiz-FpLlefia <saizangullomiguelangel@fpllefia.com>
2025-11-14 17:02:37 +0000

    Add the explciation about the pp6

---
+++ teoria/teoriapp6/index.php
@@ -0,0 +1,28 @@
+<?php
+    include 'config.php';
+    include 'data.php';
+
+    $result = returnUsersData();
+
+    echo "<table border='1' cellpadding='8' cellspacing='0'>";
+
+    echo "<tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Rol</th>
+        </tr>";
+
+    foreach($result as $user) {
+        echo "<tr>";
+        echo "<td>" . $user['id'] . "</td>";
+        echo "<td>" . htmlspecialchars($user['nom']) . "</td>";
+        echo "<td>" . htmlspecialchars($user['email']) . "</td>";
+        echo "<td>" . htmlspecialchars($user['rol']) . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+
+?>
